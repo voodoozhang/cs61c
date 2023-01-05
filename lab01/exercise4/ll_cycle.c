b123d1f4b082d61ac12a44250c27369a1e1e1d3d
voodoo <974133539@qq.com>
2023-01-05 11:15:24 +0800

    lab1

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -3,4 +3,11 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    struct node *start=head;
+    head=head->next;
+    while (head!=NULL){
+        if(head==start)return 1;
+        head=head->next;
+    }
+    return 0;
 }
